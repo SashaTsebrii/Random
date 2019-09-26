89001ba291aaa2dc5f22e863233a36ef0e499d29
SashaTsebrii <sashatsebrii@gmail.com>
2019-09-26 14:52:37 +0300

    Working commit.

--- Random/AppDelegate.m
+++ Random/AppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "AppDelegate.h"
 #import "ATStartController.h"
+#import "ATPredictionController.h"
 
 @interface AppDelegate ()
 
@@ -15,15 +16,22 @@
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     
-    ATStartController *startController = [[ATStartController alloc] init];
-    self.window.rootViewController = startController;
+    NSUInteger language = ATSelectedLanguageEnglish;
+    NSString *deviceLanguage = [[NSLocale preferredLanguages] objectAtIndex:0];
+    if ([deviceLanguage isEqualToString:@"ru"] || [deviceLanguage isEqualToString:@"uk"]) {
+        language = ATSelectedLanguageRussian;
+    } else {
+        language = ATSelectedLanguageEnglish;
+    }
+    
+    ATPredictionController *predictionController = [[ATPredictionController alloc] initWithselectedLanguage:language];
+    self.window.rootViewController = predictionController;
     
     return YES;
     
